8967e557c2a5a549a720b3d871b1582771843271
Gabriel Messias <gabrielmessias@outlook.com>
2025-01-21 22:51:34 -0300

    estudos js

---
+++ javascript/estudos/oplogico.js
@@ -0,0 +1,7 @@
+var filme = false; //testando !
+
+if (!filme) {   // a exclamação testará se o filme é null, undefined ou false.
+    console.log ("Filme inexistente.")
+} else {
+    console.log ("Você encontrou o filme.")
+}
---
+++ javascript/estudos/switch.js
@@ -0,0 +1,12 @@
+const filme = "aventura" /* "terror" , comedia */
+
+switch (filme) {
+    case "terror":
+        console.log ("Cuidado esse filme pode contem cenas de violência.")
+    break;
+    case "comedia":
+        console.log ("Esse filme irá te fazer rir.")
+    break;
+    default:
+        console.log ("Favor verificar a classificação etária do filme.")
+}
