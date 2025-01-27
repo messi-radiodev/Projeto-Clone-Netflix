4355c8cba95edde058373a5d59cee4d6cbc8e0b2
Gabriel Messias <gabrielmessias@outlook.com>
2025-01-27 20:01:36 -0300

    começando crud

--- netflix-webservice/index.js
+++ netflix-webservice/index.js
@@ -1,10 +1,38 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const mongoose = require("mongoose");
 
+
+app.use(morgan('dev'));
+
+// pegar todos os registros
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Rota funcional'});
+    res.json({ mensagem: 'Pegar todos os registros.'});
+});
+
+// pegar um registro específico.
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `Pegar um registro específico. ${id}`});
+});
+
+// criar um registro.
+app.post('/', (req, res) => {
+    const body = req.body;
+    res.json(body);
+});
+
+// atualizar um registro
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `Atualizar o registro específico ${id}`});
+});
+
+// deletar um registro específico
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `Deletar o registro específico ${id}`});
 });
 
 app.listen(3000, () => {
